d9b490a70c7e5ec0f953a0cb1055928fce23b2ee
Tofazzal26 <afranislamabir6789@gmail.com>
2024-06-02 03:01:27 +0600

    verify token and jwt token oparation finish

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 4000;
 require("dotenv").config();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgxjhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -37,6 +59,32 @@ async function run() {
       }
       const result = await UserRoleCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "356d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", {
+          sameSite: "strict",
+          httpOnly: true,
+          secure: false,
+          maxAge: 0,
+        })
+        .send({ success: true });
     });
 
     await client.db("admin").command({ ping: 1 });
